6fd29d4f7647270ff25a86b3d24d84ec2486eac5
Emanuely Tauany <102989482+EmanuelyTauany@users.noreply.github.com>
2023-10-14 02:02:28 +0000

    Mudanças 13/10

--- Projeto/script.js
+++ Projeto/script.js
@@ -1,5 +1,11 @@
-fetch('header.html')
-.then(response => response.text())
-.then(html=> {const localDoComponente = document.getElementById('local-do-componente');
- localDoComponente.innerHTML =html;
+divclicked.addEventListener("DOMContentLoaded", function(){
+    function clickMe(){
+        var divclicked= document.getElementById("quadrado");
+        
+        divclicked.addEventListener("click", function(){
+            window.location.href="palestra.html";
+
+        });
+    }
+    clickMe();
 });
